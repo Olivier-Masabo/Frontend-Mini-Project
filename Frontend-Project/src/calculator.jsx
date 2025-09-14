1feaaf43d9f61e352f30b3ca8e0d496fdcce15c0
Olivier-Masabo <masaboolivier21@gmail.com>
2025-09-14 19:00:11 +0200

    first push

---
+++ Frontend-Project/src/calculator.jsx
@@ -0,0 +1,17 @@
+function Calculator(){
+
+
+
+
+
+
+
+
+    return(
+        <>
+        <h2 className="bg-red-500 text-2xl font-bold">Hello World</h2>
+        </>
+    )
+}
+
+export default Calculator
