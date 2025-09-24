507828caad4f2b1b2012a29b7f05bf7d3ff1cc3a
Olivier-Masabo <masaboolivier21@gmail.com>
2025-09-24 22:34:29 +0200

    redesign task display

--- Frontend-Project/src/TodoList.jsx
+++ Frontend-Project/src/TodoList.jsx
@@ -20,7 +20,7 @@ import { useState } from "react";
 
     return(
         <>
-        <div className="border bg-gradient-to-l from-green-800 to-purple-800 mt-5 mb-8 p-4 text-center rounded-xl shadow-2xl shadow-gray-300 text-white font-bold text-2xl">Welcome to My Todo list app</div>
+        <div className="border bg-gradient-to-l from-green-800 to-purple-800 mt-5 mb-8 p-4 text-center rounded-xl shadow-2xl shadow-gray-300 text-white font-bold text-2xl">Welcome to My To-Do-List  app</div>
         <div className="flex gap-x-10">
          <div className="border border-gray-600 showdow-2xl shadow-neutral-600 rounded-2xl w-[30%] h-auto text-center px-5 py-7 text-white bg-gradient-to-r from-lime-800 to-blue-800">
  
@@ -37,13 +37,13 @@ import { useState } from "react";
           </ul>
         </div>
 
-        <div className="border border-gray-600 showdow-2xl shadow-neutral-600 rounded-2xl w-[60%] h-auto px-5 py-7">
-            <h1 className="font-semibold text-2xl mb-5">Create you list here</h1>
+        <div className="border border-gray-600 showdow-2xl shadow-neutral-600 rounded-2xl w-[60%] h-auto px-5 py-7 text-center">
+            <h1 className="font-semibold text-2xl mb-5">Create your list here</h1>
             <input value={newTask} onChange={handleInput} className="border border-gray-500 py-3 px-2 outline-hidden w-[45%] rounded-l-lg" type="text" placeholder="Enter task here..." />
             <button  onClick={addTask} className=" cursor-pointer border border-gray-500 py-3 px-3 bg-green-600 text-md hover:bg-green-400 text-white ">Add</button>
         <div>
 
-        <ol >
+        <ol className="w-full flex flex-col items-center">
           {task.map((element,index) => 
           <li className="bg-gray-100 shadow-xl py-3 px-2 shadow-gray-200 w-[52%] mt-3 text-lg rounded-xl flex justify-between" key={index}>
             <span>{element}</span>
